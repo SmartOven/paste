6746894c75230af6f92df535baf3747d70e6ca85
smartoven <yarik.panteleev@gmail.com>
2023-12-29 22:51:51 +0300

    fix bugs

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,7 +3,7 @@
 PASTE_SECRET_ID=e6qqq8bchnjmj39c7b26
 PROPERTIES_FILE=lockbox.properties
 
-kill -9 $(cat paste-backend.pid)
-java -jar properties-fetcher.jar $YC_OAUTH_TOKEN $PASTE_SECRET_ID $PROPERTIES_FILE
-java -jar paste.jar --spring.config.import=$PROPERTIES_FILE > logs.log &
+kill -15 $(cat paste-backend.pid)
+java -jar properties-fetcher.jar "$YC_OAUTH_TOKEN" "$PASTE_SECRET_ID" "$PROPERTIES_FILE" > properties-fetcher.log
+java -jar paste.jar --spring.config.import="$PROPERTIES_FILE" > logs.log &
 echo $! > paste-backend.pid
