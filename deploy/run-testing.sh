a33a77fe8e91498d6c67165863d11fe6229450ca
smartoven <yarik.panteleev@gmail.com>
2023-12-29 20:18:34 +0300

    v1.0

--- deploy/run-testing.sh
+++ deploy/run-testing.sh
@@ -4,4 +4,5 @@ PASTE_SECRET_ID=e6qh5b8uvsdkub755o62
 PROPERTIES_FILE=lockbox.properties
 
 java -jar properties-fetcher.jar $YC_OAUTH_TOKEN $PASTE_SECRET_ID $PROPERTIES_FILE
-java -jar paste.jar --spring.config.import=$PROPERTIES_FILE
+java -jar paste.jar --spring.config.import=$PROPERTIES_FILE > logs.log &
+echo $! > paste-backend.pid
--- deploy/run.sh
+++ deploy/run.sh
@@ -4,4 +4,5 @@ PASTE_SECRET_ID=e6qqq8bchnjmj39c7b26
 PROPERTIES_FILE=lockbox.properties
 
 java -jar properties-fetcher.jar $YC_OAUTH_TOKEN $PASTE_SECRET_ID $PROPERTIES_FILE
-java -jar paste.jar --spring.config.import=$PROPERTIES_FILE
+java -jar paste.jar --spring.config.import=$PROPERTIES_FILE > logs.log &
+echo $! > paste-backend.pid
