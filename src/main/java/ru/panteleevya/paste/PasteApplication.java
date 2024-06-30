140f4c9fd215a0923cc8c2a5f1b10a507926f1de
smartoven <yarik.panteleev@gmail.com>
2024-06-30 23:59:20 +0300

    trying to fix env vars in backend

--- src/main/java/ru/panteleevya/paste/PasteApplication.java
+++ src/main/java/ru/panteleevya/paste/PasteApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PasteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PasteApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(PasteApplication.class);
+        springApplication.addListeners(new PropertiesLogger());
+        springApplication.run(args);
     }
 
 }
