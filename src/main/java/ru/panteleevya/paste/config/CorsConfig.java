9d790eb29a99e0048a3d31f0237e5c8e757e0683
smartoven <yarik.panteleev@gmail.com>
2024-07-01 15:43:26 +0300

    fix bug

--- src/main/java/ru/panteleevya/paste/config/CorsConfig.java
+++ src/main/java/ru/panteleevya/paste/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package ru.panteleevya.paste.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,9 +12,12 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsFilter corsFilter(
+            @Value("${frontend.host}") String frontendHost,
+            @Value("${frontend.port}") String frontendPort
+    ) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(frontendHost + ":" + frontendPort));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("Content-Type"));
 
