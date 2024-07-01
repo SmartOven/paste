1626eb270dc4e80cbc7bf131458c8b7727261619
smartoven <yarik.panteleev@gmail.com>
2024-07-01 17:09:03 +0300

    fix nginx conf

--- deploy/setup_nginx.sh
+++ deploy/setup_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yes | cp -rf paste.panteleevya.ru /etc/nginx/sites-available/paste.panteleevya.ru
-sudo ln -s /etc/nginx/sites-available/vault.panteleevya.ru /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/paste.panteleevya.ru /etc/nginx/sites-enabled/
 yes | cp -rf minio.panteleevya.ru /etc/nginx/sites-available/minio.panteleevya.ru
 sudo ln -s /etc/nginx/sites-available/minio.panteleevya.ru /etc/nginx/sites-enabled/
 nginx -t
