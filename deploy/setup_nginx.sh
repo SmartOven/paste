c4bd3f7179d1cdf6e38c7a7c41dd3d243a2e6c7a
smartoven <yarik.panteleev@gmail.com>
2024-07-01 17:18:36 +0300

    fix nginx conf

--- deploy/setup_nginx.sh
+++ deploy/setup_nginx.sh
@@ -3,5 +3,7 @@ yes | cp -rf paste.panteleevya.ru /etc/nginx/sites-available/paste.panteleevya.r
 sudo ln -s /etc/nginx/sites-available/paste.panteleevya.ru /etc/nginx/sites-enabled/
 yes | cp -rf minio.panteleevya.ru /etc/nginx/sites-available/minio.panteleevya.ru
 sudo ln -s /etc/nginx/sites-available/minio.panteleevya.ru /etc/nginx/sites-enabled/
+yes | cp -rf port9000.conf /etc/nginx/sites-available/port9000.conf
+sudo ln -s /etc/nginx/sites-available/port9000.conf /etc/nginx/sites-enabled/
 nginx -t
 service nginx reload
